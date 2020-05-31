0d3b603b3372167318a9a10af6879d8e89bd97ef
Jobert Abma <jobert@hackerone.com>
2020-05-31 09:55:13 -0700

    Auto-fix Rubocop offenses

--- extract.rb
+++ extract.rb
@@ -1,18 +1,21 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 contents = ''
 
-while line = STDIN.gets
-  contents << line
+while (line = STDIN.gets)
+  contents += line
 end
 
+REGEX = %r{(^.*?("|')(/[\w\d\?/&=\#\.\!:_-]*?)(\2).*$)}.freeze
 EXCERPT_FORMAT = "------------------------------------------------\r\n%s\r\n"
 
 def sanitize_non_ascii(string)
   encoding_options = {
     invalid: :replace,
     undef: :replace,
-    replace: '_',
+    replace: '_'
   }
 
   string.encode(Encoding.find('ASCII'), **encoding_options)
@@ -20,7 +23,7 @@ end
 
 matched_endpoints = []
 
-sanitize_non_ascii(contents).gsub(/;/, "\n").scan(/(^.*?("|')(\/[\w\d\?\/&=\#\.\!:_-]*?)(\2).*$)/).map do |string|
+sanitize_non_ascii(contents).gsub(/;/, "\n").scan(REGEX).map do |string|
   next if matched_endpoints.include?(string[2])
 
   matched_endpoints << string[2]
